827d8f5e5c01eeb22c3901ada92f7b30783fe50d
Jack M. Thompson <WebKruncher@GMail.com>
2014-03-01 14:23:47 -0500

    Added license

--- x11methods.h
+++ x11methods.h
@@ -1,3 +1,29 @@
+/*
+* Copyright (c) Jack M. Thompson WebKruncher.com, exexml.com
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the WebKruncher nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY Jack M. Thompson ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL Jack M. Thompson BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 #ifndef __X11_METHODS_H__
 #define __X11_METHODS_H__
 
@@ -104,6 +130,7 @@ namespace X11Methods
 
 		virtual void Fill(Display* display,Pixmap& bitmap,GC& gc)
 		{
+			XSetForeground(display,gc,0XFFFF);
 			for (typename set<R>::iterator it=this->begin();it!=this->end();it++)
 			{
 				R& r(const_cast<R&>(*it));
